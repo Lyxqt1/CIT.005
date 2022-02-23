b589a6295f2d2513d598e3665615c3d1f750abdb
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-02-23 16:00:43 +0800

    add checkbox and pass the date into main

--- Kal/act3/src/com/kal/PackageGUI.java
+++ Kal/act3/src/com/kal/PackageGUI.java
@@ -3,11 +3,17 @@ package com.kal;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.*;
+import javax.swing.table.JTableHeader;
 
 
 public class PackageGUI extends JFrame implements ActionListener{
     static JTextField costTf;
+    static JCheckBox stdShipping;
+    static JCheckBox tdShipping;
+    static JCheckBox onShipping;
     PackageGUI(){}
     //Creating the panel at bottom and adding components
     /**
@@ -29,23 +35,33 @@ public class PackageGUI extends JFrame implements ActionListener{
         m1.add(costTf);
         m1.add(setCost);
 
-        JRadioButton r1=new JRadioButton("Standard Shipping");
-        JRadioButton r2=new JRadioButton("Two-Day Shipping");
-        JRadioButton r3=new JRadioButton("Overnight Shipping");
-        r1.setBounds(0,30,150,30);
-        r2.setBounds(150,30,150,30);
-        r3.setBounds(300, 30, 150,30);
-        JTextArea bg=new JTextArea();
-        bg.setEditable(false);
-        bg.add(r1);bg.add(r2);bg.add(r3);
-        frame.add(bg);
+        stdShipping = new JCheckBox("Standard Shipping",true);
+        tdShipping = new JCheckBox("Two-Day Shipping",false);
+        onShipping = new JCheckBox("Overnight Shipping",false);
+        stdShipping.addActionListener(pGUI);
+        tdShipping.addActionListener(pGUI);
+        onShipping.addActionListener(pGUI);
+
+        stdShipping.setBounds(0,1,150,30);
+        tdShipping.setBounds(150,1,150,30);
+        onShipping.setBounds(300, 1, 150,30);
+        JLabel bgText=new JLabel();
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(stdShipping);bg.add(tdShipping);bg.add(onShipping);
+        bgText.add(stdShipping);bgText.add(tdShipping);bgText.add(onShipping);
+
+        JTextField textW = new JTextField("weight in ounce");
+        JTextField inputW = new JTextField();
+
+
+        frame.add(bgText);
 
         //Creating the MenuBar and adding components
 
         //Adding Components to the frame.
         frame.getContentPane().add(BorderLayout.NORTH, mb);
 
-        frame.show(true);
+        frame.setVisible(true);
     }
     public static void main(String[] args) {
         mainWindow();
@@ -56,6 +72,24 @@ public class PackageGUI extends JFrame implements ActionListener{
         if (s.equals("save cost")) {
             TestingPack.packageCost= Double.parseDouble(costTf.getText());
             System.out.println(TestingPack.packageCost);
+        }
+        if(stdShipping.isSelected()){
+            tdShipping.setSelected(false);
+            onShipping.setSelected(false);
+            TestingPack.packageType=0;
+            System.out.println(TestingPack.packageType);
+        }
+        if(tdShipping.isSelected()){
+            stdShipping.setSelected(false);
+            onShipping.setSelected(false);
+            TestingPack.packageType=1;
+            System.out.println(TestingPack.packageType);
+        }
+        if(onShipping.isSelected()){
+            stdShipping.setSelected(false);
+            tdShipping.setSelected(false);
+            TestingPack.packageType=2;
+            System.out.println(TestingPack.packageType);
         }
     }
 }
--- Kal/act3/src/com/kal/TestingPack.java
+++ Kal/act3/src/com/kal/TestingPack.java
@@ -2,6 +2,7 @@ package com.kal;
 
 public class TestingPack {
     public static double packageCost = 69.42D;
+    public static int packageType = 0;
     public static void main(String[] args) {
         PackageGUI.mainWindow();
     }
